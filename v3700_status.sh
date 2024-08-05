6573140fc92d0d715967466235505c09bf177b44
Marc Boucher <marc.boucher@saq.qc.ca>
2024-08-05 17:08:25 -0400

    Adding ssh config file + params ajustements

--- v3700_status.sh
+++ v3700_status.sh
@@ -6,7 +6,7 @@
 # IBM DSxx health monitoring
 # 
 # Usage:  v3700_status.sh <DS_controller> OPTIONS
-# Requirements: SSH Key-Based Authentication in place
+# Requirements: SSH key authentication in place
 # Params:
 #         $1 V3700 ip / hostname
 #         $2 parameter to measure (lsarray, lsdrive)
@@ -57,7 +57,7 @@ fi
 #
 
 if [[ $2 == 'discoverdrives' ]]; then
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsdrive -nohdr -delim :)' )
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsdrive -nohdr -delim :)' )
   count=0
   echo "["
   for line in ${RESULT//\\n/ }
@@ -74,7 +74,7 @@ if [[ $2 == 'discoverdrives' ]]; then
 fi
 
 if [[ $2 == 'discoverenclosures' ]]; then
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsenclosure -nohdr -delim :)' )
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosure -nohdr -delim :)' )
   count=0
   echo "["
   for line in ${RESULT//\\n/ }
@@ -91,7 +91,7 @@ if [[ $2 == 'discoverenclosures' ]]; then
 fi
 
 if [[ $2 == 'discoverarrays' ]]; then
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsarray -nohdr -delim :)' )
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsarray -nohdr -delim :)' )
   count=0
   echo "["
   for line in ${RESULT//\\n/ }
@@ -108,7 +108,7 @@ if [[ $2 == 'discoverarrays' ]]; then
 fi
 
 if [[ $2 == 'discovervdisks' ]]; then
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsvdisk -nohdr -delim :)' )
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsvdisk -nohdr -delim :)' )
   count=0
   echo "["
   for line in ${RESULT//\\n/ }
@@ -127,8 +127,8 @@ fi
 
 #lsdrive -> state of drives
 if [[ $2 == 'lsdrive' ]]; then
-  BUSCA=$3
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 "( lsdrive -filtervalue enclosure_id=$3:slot_id=$4 -delim : -nohdr )" )
+  BUSCA=$3 
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 "( lsdrive -filtervalue enclosure_id=$3:id=$4 -delim : -nohdr )" )
   while IFS=':' read -ra fields; do
     echo "${fields[1]}"
   done <<< "$RESULT"
@@ -137,8 +137,8 @@ fi
 
 #lsarray -> state of array MDisks
 if [[ $2 == 'lsarray' ]]; then
-  BUSCA=$3
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsarray )' )
+  BUSCA=$3 
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsarray )' )
   # echo "$RESULT" | awk '$1=='"$BUSCA"' {print $3}'
   if [ -z "$4" ]
   then
@@ -155,8 +155,8 @@ fi
 
 #lsvdisk -> state of volumes (lun's/vdisk's)
 if [[ $2 == 'lsvdisk' ]]; then
-  BUSCA=$3
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsvdisk )' )
+  BUSCA=$3 
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsvdisk )' )
   #echo "$RESULT" | awk '$1=='"$BUSCA"' {print $3}'
   if [ -z "$4" ]
   then
@@ -172,8 +172,8 @@ fi
 
 # lsenclosure -> state of enclosures
 if [[ $2 == 'lsenclosure' ]]; then
-  BUSCA=$3
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsenclosure )' )
+  BUSCA=$3 
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosure )' )
   echo "$RESULT" | awk '$1=='"$BUSCA"' {print $2}'
 fi
 
@@ -181,7 +181,7 @@ fi
 if [[ $2 == 'lsenclosurebattery' ]]; then
   BUSCA=$3
   BUSCA2=$4
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsenclosurebattery )' )
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosurebattery )' )
   #echo "$RESULT" | awk '$1=='"$BUSCA"' {print $3}'
   echo "$RESULT" | awk '$1=='"$BUSCA"' && $2=='"$BUSCA2"'  {print $3}'
 fi
@@ -190,7 +190,7 @@ fi
 if [[ $2 == 'lsenclosurecanister' ]]; then
   BUSCA=$3
   BUSCA2=$4
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsenclosurecanister )' )
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosurecanister )' )
   #echo "$RESULT" | awk '$1=='"$BUSCA"' && $2=='"$BUSCA2"'  {print $3 ";" $4}'
   echo "$RESULT" | awk '$1=='"$BUSCA"' && $2=='"$BUSCA2"'  {print $3}'
 
@@ -198,22 +198,22 @@ fi
 
 # lsenclosurepsu -> state of psu's
 if [[ $2 == 'lsenclosurepsu' ]]; then
-  BUSCA=$3
+  BUSCA=$3 
   BUSCA2=$4
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsenclosurepsu )' )
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosurepsu )' )
   echo "$RESULT" | awk '$1=='"$BUSCA"' && $2=='"$BUSCA2"'  {print $3}'
 fi
 
 # lsenclosureslot -> state of port enclosure slots
 # Muestra yes o no dependiendo de si el disco esta presente o no, si hay mas de un enclosure no se como va
 if [[ $2 == 'lsenclosureslot' ]]; then
-  BUSCA=$3
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lsenclosureslot )' )
+  BUSCA=$3 
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosureslot )' )
   echo "$RESULT" | awk '$2=='"$BUSCA"' {print $3}'
 fi
 
 if [[ $2 == 'lsenclosureslotextended' ]]; then
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 "( lsenclosureslot -slot $3 $4 )" )
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 "( lsenclosureslot -slot $3 $4 )" )
   echo "$RESULT" | grep "drive_present" | cut -f2- -d" "
   #echo "$RESULT"
 fi
@@ -222,15 +222,15 @@ fi
 
 # logerror -> event log most important
 if [[ $2 == 'logerror' ]]; then
-  BUSCA=$3
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lseventlog -expired no -fixed no -monitoring no -message no -order severity )' )
+  BUSCA=$3 
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lseventlog -expired no -fixed no -monitoring no -message no -order severity )' )
   echo "$RESULT"
 fi
 
 # logerrorall -> event log all
 if [[ $2 == 'logerrorall' ]]; then
-  BUSCA=$3
-  RESULT=$(ssh -o StrictHostKeyChecking=no $1 '( lseventlog )' )
+  BUSCA=$3 
+  RESULT=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lseventlog )' )
   echo "$RESULT"
 fi
 
@@ -243,35 +243,35 @@ if [[ $2 == 'logstatus' ]]; then
 	RESULT+=lsdrive___$NOW__$NOWT
 	RESULT+=$(echo \\n--\\n)
 	RESULT+=$(echo --)
-	RESULT+=$(sshpass -p $3 ssh -o StrictHostKeyChecking=no $2@$1 '( lsdrive)' )
+	RESULT+=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsdrive)' )
 	RESULT+=lsarray___$NOW__$NOWT
 	RESULT+=$(echo --)
 	RESULT+=$(echo --)
-	RESULT+=$(sshpass -p $3 ssh -o StrictHostKeyChecking=no $2@$1 '( lsarray)' )
+	RESULT+=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsarray)' )
 	RESULT+=lsvdisk___$NOW__$NOWT
 	RESULT+=$(echo --)
 	RESULT+=$(echo --)
-	RESULT+=$(sshpass -p $3 ssh -o StrictHostKeyChecking=no $2@$1 '( lsvdisk)' )
+	RESULT+=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsvdisk)' )
 	RESULT+=lsenclosure___$NOW__$NOWT
 	RESULT+=$(echo --)
 	RESULT+=$(echo --)
-	RESULT+=$(sshpass -p $3 ssh -o StrictHostKeyChecking=no $2@$1 '( lsenclosure)' )
+	RESULT+=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosure)' )
 	RESULT+=lsenclosurebattery___$NOW__$NOWT
 	RESULT+=$(echo --)
 	RESULT+=$(echo --)
-	RESULT+=$(sshpass -p $3 ssh -o StrictHostKeyChecking=no $2@$1 '( lsenclosurebattery)' )
+	RESULT+=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosurebattery)' )
 	RESULT+=lsenclosurepsu___$NOW__$NOWT
 	RESULT+=$(echo --)
 	RESULT+=$(echo --)
-	RESULT+=$(sshpass -p $3 ssh -o StrictHostKeyChecking=no $2@$1 '( lsenclosurepsu)' )
+	RESULT+=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosurepsu)' )
 	RESULT+=lsenclosureslot___$NOW__$NOWT
 	RESULT+=$(echo --)
 	RESULT+=$(echo --)
-	RESULT+=$(sshpass -p $3 ssh -o StrictHostKeyChecking=no $2@$1 '( lsenclosureslot)' )
+	RESULT+=$(ssh -F /var/lib/zabbix/ssh_keys/config -o StrictHostKeyChecking=no $1 '( lsenclosureslot)' )
 	RESULT+=lsenclosureslotx___$NOW__$NOWT
 	RESULT+=$(echo --)
 	RESULT+=$(echo --)
-	echo "$RESULT"
+	echo "$RESULT" 
 fi
 
 ### DEBUG ###
